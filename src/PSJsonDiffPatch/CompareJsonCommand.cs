aeaa711fa13b02167c22f579244cdc7a3148f2a9
Sean McArdle <sean@mcardletech.com>
2022-04-25 11:27:41 -0700

    Added a test. Not passing, still working on that.

---
+++ src/PSJsonDiffPatch/CompareJsonCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+using JsonDiffPatchDotNet;
+using Newtonsoft.Json.Linq;
+
+namespace PSJsonDiffPatch
+{
+    
+    [Cmdlet(VerbsData.Compare, "Json")]
+    [OutputType(typeof(JToken))]
+    public class CompareJsonCommand : PSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName=true)]
+        public string FirstRecord { get; set; }
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = true)]
+        public string SecondRecord { get; set; }
+
+        private JsonDiffPatch jdp;
+
+        protected override void BeginProcessing() {
+            jdp = new JsonDiffPatch();
+        }
+
+
+        protected override void ProcessRecord() {
+            WriteObject(jdp?.Diff(FirstRecord, SecondRecord));
+        }
+
+        protected override void EndProcessing ()
+        {
+
+        }
+    }
+}
---
+++ src/PSJsonDiffPatch/Tests/Compare-Json.Tests.ps1
@@ -0,0 +1,20 @@
+﻿#Requires -Modules Pester, PSJsonDiffPatch
+
+
+
+
+
+Describe 'Compare-Json' {
+    BeforeAll {
+	    $docOne = "{'Foo':'Bar', 'Baz':[1,2,3]}"
+
+	    $docTwo = "{'Foo':'Bar', 'Baz':[1,2,3,4]}"
+
+	    $docThree = "{'Foo':'Bar', 'Baz':[1,2,3,4]}"
+    }
+
+	It 'Given docOne and docTwo, the Baz element should differ.' {
+		$result = Compare-Json $docOne $docTwo
+		$result | Should -Not -Be $null
+	}
+}
